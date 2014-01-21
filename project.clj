6f5894a4089ecded1f5d43c12d0361cf5e410347
Nick Zbinden <nickik@gmx.ch>
2014-01-21 02:08:53 +0100

    exeriment with liberator and compjure

--- project.clj
+++ project.clj
@@ -4,4 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.match "0.2.1"]])
+                 [org.clojure/core.match "0.2.1"]
+                 [liberator "0.10.0"]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.4"]]
+  :plugins [[lein-ring "0.8.10"]]
+  :ring {:handler stratego-core.server/handler
+         :port 80
+         :auto-reload? true
+         :auto-refresh? true})
--- src/stratego_core/server.clj
+++ src/stratego_core/server.clj
@@ -1,19 +1,47 @@
 (ns stratego-core.server
-  (:require [clojure.core.match :as m])
+  (:require [clojure.core.match :as m]
+            [compojure.core :refer [defroutes ANY]])
   (:use [clojure.pprint]
-        [stratego-core.core]))
+        [stratego-core.core]
+        [liberator.core :only [defresource request-method-in]]))
 
 (def games (atom {}))
 
 (defn uuid [] (str (java.util.UUID/randomUUID)))
 
-(defn new-game [games start-field]
-  (let [uuid (uuid)]
-    (do
-      (swap! games assoc uuid full-empty-board)
-      uuid)))
+(defn new-game
+  ([] (new-game full-empty-board))
+  ([start-field]
+   (let [uuid (uuid)]
+     (do
+       (swap! games assoc uuid {:board start-field})
+       uuid))))
+
+(defresource main
+  :available-media-types ["text/html"]
+  :handle-ok (str  "<h3> Stratego Server </h3>
+                   <p> Play the old school board game stratego agaisnt
+                   your friends or enemys. </p>
+                   <p> Source Code and Documnetation is here:
+                   <a href=\")https://github.com/nickik/stratego-core\"> stratego-core </a> </p>
+                   <p> Allready existing games:"  (keys @games) "</p>"))
 
 
+(defresource game
+  :allowed-methods [:post :get]
+  :available-media-types ["text/html"]
+  :handle-ok (fn [ctx]
+               (str "Any Game " (keys @games)))
+  :post! (fn [ctx]  (new-game))
+
+  :post-redirect? false)
+
+
+(defroutes app
+  (ANY "/" [] main)
+  (ANY "/game" [] game))
+
+(def handler (-> app))
 
 ;; From Player 1: POST game/<start-field> --> /game/uuid
 
